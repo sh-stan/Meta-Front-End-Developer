d8ea7171fa1d1b32399e79935198659219b6466c
Stan Shishmanov <slaaff@protonmail.com>
2023-10-05 09:38:28 +0300

    Added Basic React Course - first 5 labs

---
+++ Meta Front End Developer/5. React Basics/4. Lab - Passing Props/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName='Charlie'/>
+            <Heading firstName='Robert'/>
+        </div>
+    );
+};
+
+export default App;
